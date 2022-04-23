0020b8ef373e7c0deefa15cc7f4db5837a7b203e
LonziSlovakia <lonzikormos@gmail.com>
2022-04-23 19:20:41 +0200

    Created components for cards

--- child_app/lib/widgets/home/home.dart
+++ child_app/lib/widgets/home/home.dart
@@ -1,6 +1,8 @@
+import 'package:child_app/widgets/content/content.dart';
 import 'package:flutter/material.dart';
 
 import '../todos/todos.dart';
+import '../balance/balance.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key, required this.title}) : super(key: key);
@@ -30,9 +32,21 @@ class _HomePageState extends State<HomePage> {
       body: Center(
         child: Column(
           children: const <Widget>[
-            HomeCard(title: "Todos", routeName: Todos.routeName),
-            HomeCard(title: "Todos", routeName: Todos.routeName),
-            HomeCard(title: "Todos", routeName: Todos.routeName),
+            HomeCard(
+                title: "💰 Balance 💰",
+                routeName: Todos.routeName,
+                content: PiggyBank(
+                  money: 100,
+                  coins: 10,
+                )),
+            HomeCard(
+                title: "✅ Missions ✅",
+                routeName: Todos.routeName,
+                content: ThumbnailContent(title: "Lóve")),
+            HomeCard(
+                title: "👨‍👩‍👧‍👦 Close people 👨‍👩‍👧‍👦",
+                routeName: Todos.routeName,
+                content: ThumbnailContent(title: "Lóve")),
           ],
         ),
       ), // This trailing comma makes auto-formatting nicer for build methods.
@@ -41,38 +55,47 @@ class _HomePageState extends State<HomePage> {
 }
 
 class HomeCard extends StatelessWidget {
-  const HomeCard({Key? key, required this.title, required this.routeName}) : super(key: key);
+  const HomeCard(
+      {Key? key,
+      required this.title,
+      required this.routeName,
+      required this.content})
+      : super(key: key);
 
   final String title;
   final String routeName;
+  final Widget content;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
         onTap: () {
-      Navigator.pushNamed(context, routeName);
-    },
-    child: Container(
-      width: double.infinity,
-      child: Card(
-          margin: const EdgeInsets.fromLTRB(20, 20, 20, 0),
-          color: Colors.grey,
-          child: Container(
-              padding: EdgeInsets.all(20),
-              child: Column(
-                children: [
-                  Text(
-                    'You have pushed the button this many times:',
-                  ),
-                  Text(
-                    'nic',
-                    style: Theme.of(context).textTheme.headline4,
-                  ),
-                ],
-              )
-          )
-      ),
-    )
-    );
+          Navigator.pushNamed(context, routeName);
+        },
+        child: Container(
+          width: double.infinity,
+          child: Card(
+              margin: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+              color: Colors.grey,
+              child: Container(
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          title,
+                          textAlign: TextAlign.left,
+                          style: Theme.of(context).textTheme.headline6,
+                        ),
+                      ),
+                      Divider(
+                        height: 20,
+                        color: Colors.transparent,
+                      ),
+                      content,
+                    ],
+                  ))),
+        ));
   }
 }
