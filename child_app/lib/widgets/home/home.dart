487a955d8931e8b3bfb9134d80872e875ba6953f
Samuel Manik <maniksamuel159@gmail.com>
2022-04-23 14:47:31 +0200

    make HomeCard

--- child_app/lib/widgets/home/home.dart
+++ child_app/lib/widgets/home/home.dart
@@ -29,36 +29,50 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Center(
         child: Column(
-          children: <Widget>[
-            GestureDetector(
-                onTap: () {
-                  Navigator.pushNamed(context, Todos.routeName);
-                },
-                child: Container(
-                    width: double.infinity,
-                    child: Card(
-                        margin: EdgeInsets.all(20),
-                        color: Colors.grey,
-                        child: Container(
-                            padding: EdgeInsets.all(20),
-                            child: Column(
-                              children: [
-                                Text(
-                                  'You have pushed the button this many times:',
-                                ),
-                                Text(
-                                  '$_counter',
-                                  style: Theme.of(context).textTheme.headline4,
-                                ),
-                              ],
-                            )
-                        )
-                    )
-                )
-            )
+          children: const <Widget>[
+            HomeCard(title: "Todos", routeName: Todos.routeName),
+            HomeCard(title: "Todos", routeName: Todos.routeName),
+            HomeCard(title: "Todos", routeName: Todos.routeName),
           ],
         ),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class HomeCard extends StatelessWidget {
+  const HomeCard({Key? key, required this.title, required this.routeName}) : super(key: key);
+
+  final String title;
+  final String routeName;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: () {
+      Navigator.pushNamed(context, routeName);
+    },
+    child: Container(
+      width: double.infinity,
+      child: Card(
+          margin: EdgeInsets.all(20),
+          color: Colors.grey,
+          child: Container(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                children: [
+                  Text(
+                    'You have pushed the button this many times:',
+                  ),
+                  Text(
+                    'nic',
+                    style: Theme.of(context).textTheme.headline4,
+                  ),
+                ],
+              )
+          )
+      ),
+    )
     );
   }
 }
