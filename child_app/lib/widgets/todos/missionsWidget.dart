ca35c5212b3f8732bf17d9ec6b49dc88e8fbc5df
Samuel Manik <maniksamuel159@gmail.com>
2022-04-24 02:37:54 +0200

    add mark as complete in backend

--- child_app/lib/widgets/todos/missionsWidget.dart
+++ child_app/lib/widgets/todos/missionsWidget.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:http/http.dart' as http;
 import '../../util/AppColors.dart';
 import '../Loading/Loading.dart';
 
@@ -29,8 +29,13 @@ class _MissionsWidgetState extends State<MissionsWidget> {
                       return Dismissible(
                           direction: DismissDirection.endToStart,
                           confirmDismiss: (direction) async {
+                            await http.patch(
+                              Uri.parse(LoadingState.apiUrl +
+                                  "/mission/${missions[index].id}/complete"),
+                            );
+                            print("Mission ${missions[index].id} completed");
                             setState(() {
-                              //missions.remove(missions.elementAt(index));
+                              missions.removeAt(index);
                             });
                             return false;
                           },
@@ -58,68 +63,72 @@ class _MissionsWidgetState extends State<MissionsWidget> {
                               ),
                               margin: EdgeInsets.only(
                                   top: 10, bottom: 0, left: 20, right: 20),
-                              color: missions.elementAt(index).state == "created" ? Color(0xFFf2f2f2) :
-                              missions.elementAt(index).state == "completed" ? (AppColors.completeColor) :
-                              Color(0xFFf2f2f2),
+                              color:
+                                  missions.elementAt(index).state == "created"
+                                      ? Color(0xFFf2f2f2)
+                                      : missions.elementAt(index).state ==
+                                              "completed"
+                                          ? (AppColors.completeColor)
+                                          : Color(0xFFf2f2f2),
                               shadowColor: Colors.transparent,
-                              child: missions.elementAt(index)
-                                          .state ==
-                                      "created"
-                                  ? (ListTile(
-                                      title: Text(
-                                        missions.elementAt(index).text,
-                                        style: TextStyle(
-                                          fontSize: 20,
-                                          color: AppColors.grey,
-                                          fontWeight: FontWeight.bold,
-                                          fontFamily: 'Inter',
-                                        ),
-                                      ),
-                                      subtitle: Text(
-                                        missions
-                                            .elementAt(index)
-                                            .due,
-                                        style: TextStyle(
-                                          fontSize: 15,
-                                          color: Colors.grey,
-                                        ),
-                                      ),
-                                      trailing: Text(
-                                        missions
-                                            .elementAt(index).reward.values.elementAt(1).toString() + "\$",
-                                        style: TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          fontFamily: 'Inter',
-                                          fontSize: 30,
-                                          color: AppColors.grey,
-                                        ),
-                                      ),
-                                    ))
-                                  : missions
-                                              .elementAt(index)
-                                              .state ==
-                                          "completed"
-                                      ? ListTile(
+                              child:
+                                  missions.elementAt(index).state == "created"
+                                      ? (ListTile(
                                           title: Text(
                                             missions.elementAt(index).text,
                                             style: TextStyle(
                                               fontSize: 20,
-                                              color: Colors.white,
+                                              color: AppColors.grey,
                                               fontWeight: FontWeight.bold,
                                               fontFamily: 'Inter',
                                             ),
                                           ),
+                                          subtitle: Text(
+                                            missions.elementAt(index).due,
+                                            style: TextStyle(
+                                              fontSize: 15,
+                                              color: Colors.grey,
+                                            ),
+                                          ),
                                           trailing: Text(
-                                            "Claim",
+                                            missions
+                                                    .elementAt(index)
+                                                    .reward
+                                                    .values
+                                                    .elementAt(1)
+                                                    .toString() +
+                                                "\$",
                                             style: TextStyle(
                                               fontWeight: FontWeight.bold,
                                               fontFamily: 'Inter',
                                               fontSize: 30,
-                                              color: Colors.white54,
+                                              color: AppColors.grey,
                                             ),
                                           ),
-                                        )
-                                      : null ));
+                                        ))
+                                      : missions.elementAt(index).state ==
+                                              "approved"
+                                          ? ListTile(
+                                              title: Text(
+                                                missions.elementAt(index).text,
+                                                style: TextStyle(
+                                                  fontSize: 20,
+                                                  color: Colors.white,
+                                                  fontWeight: FontWeight.bold,
+                                                  fontFamily: 'Inter',
+                                                ),
+                                              ),
+                                              trailing: Text(
+                                                "Claim",
+                                                style: TextStyle(
+                                                  fontWeight: FontWeight.bold,
+                                                  fontFamily: 'Inter',
+                                                  fontSize: 30,
+                                                  color: Colors.white54,
+                                                ),
+                                              ),
+                                            )
+                                          : null));
                     },
                   ),
                 ),
